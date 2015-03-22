281cd58d9f5143886bd9048707ca58be284f03f9
David Steuber <dsteuber@gmail.com>
2015-03-22 17:59:40 -0400

    CC license for site
    Creative Commons rocks!

--- footer.php
+++ footer.php
@@ -9,6 +9,8 @@
  * @since Twenty Fifteen 1.0
  */
 ?>
+<hr />
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">David Steuber dot Com</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://www.david-steuber.com" property="cc:attributionName" rel="cc:attributionURL">David Steuber</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
 
 	</div><!-- .site-content -->
 
